09722c9b6c99e3b571dbbb7c6ddb1cd24ae604ea
ASUS <ptharanan@gmail.com>
2025-11-05 18:54:19 +0530

    Add initial Django app structure with Sample model and admin registration

--- First/First/settings.py
+++ First/First/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^@&kxkccdeh2y-%pkpzjiqe#9#gr+699jwlwui!(thi$^se+21
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'Try',
 ]
 
 MIDDLEWARE = [
---
+++ First/Try/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Sample(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    age = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.name
+    
