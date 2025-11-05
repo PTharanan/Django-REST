ed6fbbcb83d29bbfa0b9aaed585b0ed28a856997
ASUS <ptharanan@gmail.com>
2025-11-05 20:17:14 +0530

    Implement SampleAPI with POST and GET methods for Sample model

--- First/Try/views.py
+++ First/Try/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Sample
 
-# Create your views here.
+class SampleAPI(APIView):
+    
+    def post(self, request):
+        
+        print(request.data) # termilin la data va print
+        
+        new_data = Sample(name = request.data[ 'name' ], age = request.data[ 'age' ]) # pus data to model
+
+        new_data.save() # save data in databse
+
+        return Response("Sucessfully POSTED") # post panna out mess
+
+    def get(self, request):
+
+        all_data = Sample.objects.all()
+
+        data_list = []
+
+        for value in all_data:
+
+            data_dict = {
+                "name" : value.name,
+                "age" : value.age
+            }
+
+            data_list.append(data_dict)
+
+        return Response(data_list)
